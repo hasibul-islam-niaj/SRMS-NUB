573fdd672a58b9c0adc4b830db867e2685e796e8
hasibul islam <hasibul.islam@brainstation-23.com>
2022-12-11 12:51:39 +0600

    Implemented students

--- main.c
+++ main.c
@@ -7,20 +7,19 @@
 #define FILE_MENUS "../data/menus.txt"
 #define FILE_PROGRAMS "../data/programs.txt"
 #define FILE_SEMESTERS "../data/semesters.txt"
+#define FILE_STUDENTS "../data/students.txt"
 
 /*Structure Definition Section*/
 char pathOfFiles[] = "../data/";
 
-struct SubMenu
-{
+struct SubMenu {
     int sequence;
     char name[30];
     struct SubMenu *next;
 };
 typedef struct SubMenu SubMenu;
 
-struct Menu
-{
+struct Menu {
     int sequence;
     char name[30];
     SubMenu *subMenus;
@@ -29,8 +28,7 @@ struct Menu
 typedef struct Menu Menu;
 Menu *db_menus;
 
-struct ResultGrade
-{
+struct ResultGrade {
     int sequence;
     char name[2];
     float point;
@@ -40,8 +38,7 @@ struct ResultGrade
 typedef struct ResultGrade ResultGrade;
 ResultGrade *db_grades;
 
-struct Program
-{
+struct Program {
     int sequence;
     char code[20];
     char name[100];
@@ -50,8 +47,7 @@ struct Program
 typedef struct Program Program;
 Program *db_programs;
 
-struct Semester
-{
+struct Semester {
     int sequence;
     char name[50];
     struct Semester *next;
@@ -59,8 +55,7 @@ struct Semester
 typedef struct Semester Semester;
 Semester *db_semesters;
 
-struct Course
-{
+struct Course {
     int sequence;
     char code[50];
     char name[100];
@@ -70,11 +65,20 @@ struct Course
 };
 typedef struct Course Course;
 Course *db_courses;
+
+struct Student {
+    char id[20];
+    char name[50];
+    int programSequence;
+    Program *program;
+    struct Student *next;
+};
+typedef struct Student Student;
+Student *db_students;
 /*Structure Definition Section*/
 
 /*Function Definition Section*/
-void welcomeMessage()
-{
+void welcomeMessage() {
     printf("\n\tStudent Result Management System\n");
     printf("\t Northern University Bangladesh\n");
     for (int i = 0; i < 39; i++)
@@ -82,20 +86,18 @@ void welcomeMessage()
     printf("\n");
 }
 
-void waitForClick(){
+void waitForClick() {
     printf("\nPress any key to continue...");
     getchar();
     getchar();
 }
 
-void underConstruction()
-{
+void underConstruction() {
     printf("System is under Construction.\n");
     waitForClick();
 }
 
-FILE *getFile(char fileName[])
-{
+FILE *getFile(char fileName[]) {
     FILE *file;
     file = fopen(fileName, "r");
 
@@ -105,49 +107,37 @@ FILE *getFile(char fileName[])
     return file;
 }
 
-void initMenu()
-{
+void initMenu() {
     FILE *file = getFile(FILE_MENUS);
 
-    if (file != NULL)
-    {
+    if (file != NULL) {
         Menu *tempMenu = NULL;
         SubMenu *tempSubMenu = NULL;
 
-        while (!feof(file))
-        {
+        while (!feof(file)) {
             Menu *menu = malloc(sizeof(Menu));
             int isMenu;
 
             fscanf(file, "%d, %[^,], %d", &menu->sequence, menu->name, &isMenu);
-            if (isMenu)
-            {
-                if (tempMenu == NULL)
-                {
+            if (isMenu) {
+                if (tempMenu == NULL) {
                     tempMenu = menu;
                     if (db_menus == NULL)
                         db_menus = tempMenu;
-                }
-                else
-                {
+                } else {
                     tempMenu->next = menu;
                     tempMenu = tempMenu->next;
                 }
 
                 tempSubMenu = NULL;
-            }
-            else
-            {
-                if (tempSubMenu == NULL)
-                {
-                    tempSubMenu = (SubMenu *)menu;
+            } else {
+                if (tempSubMenu == NULL) {
+                    tempSubMenu = (SubMenu *) menu;
 
                     if (tempMenu->subMenus == NULL)
                         tempMenu->subMenus = tempSubMenu;
-                }
-                else
-                {
-                    tempSubMenu->next = (SubMenu *)menu;
+                } else {
+                    tempSubMenu->next = (SubMenu *) menu;
                     tempSubMenu = tempSubMenu->next;
                 }
             }
@@ -155,25 +145,19 @@ void initMenu()
     }
 }
 
-void initGrades()
-{
+void initGrades() {
     FILE *file = getFile(FILE_GRADES);
-    if (file != NULL)
-    {
+    if (file != NULL) {
         ResultGrade *tempGrade = NULL;
 
-        while (!feof(file))
-        {
+        while (!feof(file)) {
             ResultGrade *grade = malloc(sizeof(ResultGrade));
             fscanf(file, "%d, %[^,], %f, %[^\n]s", &grade->sequence, grade->name, &grade->point, grade->description);
 
-            if (db_grades == NULL)
-            {
+            if (db_grades == NULL) {
                 db_grades = grade;
                 tempGrade = grade;
-            }
-            else
-            {
+            } else {
                 tempGrade->next = grade;
                 tempGrade = grade;
             }
@@ -181,26 +165,20 @@ void initGrades()
     }
 }
 
-void initPrograms()
-{
+void initPrograms() {
     FILE *file = getFile(FILE_PROGRAMS);
 
-    if (file != NULL)
-    {
+    if (file != NULL) {
         Program *tempProgram = NULL;
 
-        while (!feof(file))
-        {
+        while (!feof(file)) {
             Program *program = malloc(sizeof(Program));
             fscanf(file, "%d, %[^,], %[^\n]s", &program->sequence, program->code, program->name);
 
-            if (db_programs == NULL)
-            {
+            if (db_programs == NULL) {
                 db_programs = program;
                 tempProgram = program;
-            }
-            else
-            {
+            } else {
                 tempProgram->next = program;
                 tempProgram = program;
             }
@@ -208,26 +186,20 @@ void initPrograms()
     }
 }
 
-void initSemesters()
-{
+void initSemesters() {
     FILE *file = getFile(FILE_SEMESTERS);
 
-    if (file != NULL)
-    {
+    if (file != NULL) {
         Semester *tempSemester = NULL;
 
-        while (!feof(file))
-        {
+        while (!feof(file)) {
             Semester *semester = malloc(sizeof(Semester));
             fscanf(file, "%d, %[^\n]s", &semester->sequence, semester->name);
 
-            if (db_semesters == NULL)
-            {
+            if (db_semesters == NULL) {
                 db_semesters = semester;
                 tempSemester = semester;
-            }
-            else
-            {
+            } else {
                 tempSemester->next = semester;
                 tempSemester = semester;
             }
@@ -235,27 +207,21 @@ void initSemesters()
     }
 }
 
-void initCourses()
-{
+void initCourses() {
     FILE *file = getFile(FILE_COURSES);
 
-    if (file != NULL)
-    {
+    if (file != NULL) {
         Course *tempCourse = NULL;
 
-        while (!feof(file))
-        {
+        while (!feof(file)) {
             Course *course = malloc(sizeof(Course));
             fscanf(file, "%d, %[^,], %[^|]| %d, %d", &course->sequence, course->code, course->name, &course->credit,
                    &course->programSequence);
 
-            if (db_courses == NULL)
-            {
+            if (db_courses == NULL) {
                 db_courses = course;
                 tempCourse = course;
-            }
-            else
-            {
+            } else {
                 tempCourse->next = course;
                 tempCourse = course;
             }
@@ -263,17 +229,49 @@ void initCourses()
     }
 }
 
-void systemInitialization()
-{
+void initStudents() {
+    FILE *file = getFile(FILE_STUDENTS);
+
+    if (file != NULL) {
+        Student *tempStudent = NULL;
+        Program *selectedProgram = NULL;
+
+        while (!feof(file)) {
+            Student *student = malloc(sizeof(Student));
+            fscanf(file, "%d, %[^,], %[^\n]s", &student->programSequence, student->id, student->name);
+
+            if (selectedProgram == NULL || selectedProgram->sequence != student->programSequence) {
+                selectedProgram = db_programs;
+                while (selectedProgram != NULL && selectedProgram->sequence != student->programSequence) {
+                    selectedProgram = selectedProgram->next;
+                }
+
+//                if (selectedProgram == NULL)
+//                    printf("\n Wrong program for student %s - %s\n", student->id, student->name);
+            }
+
+            student->program = selectedProgram;
+            if (db_students == NULL) {
+                db_students = student;
+                tempStudent = student;
+            } else {
+                tempStudent->next = student;
+                tempStudent = student;
+            }
+        }
+    }
+}
+
+void systemInitialization() {
     initMenu();
     initGrades();
     initPrograms();
     initSemesters();
     initCourses();
+    initStudents();
 }
 
-Menu *getByMenuSequence(int menuSequence)
-{
+Menu *getByMenuSequence(int menuSequence) {
     Menu *tempMenu = db_menus;
     while (tempMenu != NULL && tempMenu->sequence != menuSequence)
         tempMenu = tempMenu->next;
@@ -281,8 +279,7 @@ Menu *getByMenuSequence(int menuSequence)
     return tempMenu;
 }
 
-SubMenu *getBySubMenuSequence(Menu *menu, int subMenuSequence)
-{
+SubMenu *getBySubMenuSequence(Menu *menu, int subMenuSequence) {
     SubMenu *tempSubmenu = menu->subMenus;
     while (tempSubmenu != NULL && tempSubmenu->sequence != subMenuSequence)
         tempSubmenu = tempSubmenu->next;
@@ -290,126 +287,108 @@ SubMenu *getBySubMenuSequence(Menu *menu, int subMenuSequence)
     return tempSubmenu;
 }
 
-void showMenus()
-{
+void showMenus() {
     welcomeMessage();
     Menu *tempMenu = db_menus;
 
-    while (tempMenu != NULL)
-    {
+    while (tempMenu != NULL) {
         printf("[%d] for '%s'\n", tempMenu->sequence, tempMenu->name);
         tempMenu = tempMenu->next;
     }
 }
 
-void showSubMenus(Menu *menu)
-{
+void showSubMenus(Menu *menu) {
     SubMenu *tempSubMenu = menu->subMenus;
-    while (tempSubMenu != NULL)
-    {
+    while (tempSubMenu != NULL) {
         printf("\t[%d] for '%s'\n", tempSubMenu->sequence, tempSubMenu->name);
         tempSubMenu = tempSubMenu->next;
     }
 }
 
-void exitProgram()
-{
+void exitProgram() {
     exit(0);
 }
 
-void viewAllGrades()
-{
+void viewAllGrades() {
     ResultGrade *tempGrades = db_grades;
     printf("Sl. \tLetter Grade \tPoint \tDescription\n");
-    while (tempGrades != NULL)
-    {
+    while (tempGrades != NULL) {
         printf("[%d] \t%s \t\t\t\t%.2f \t%s\n", tempGrades->sequence, tempGrades->name, tempGrades->point,
                tempGrades->description);
         tempGrades = tempGrades->next;
     }
 }
 
-void viewAllPrograms()
-{
+void viewAllPrograms() {
     Program *tempPrograms = db_programs;
     printf("Sl. \tProgram\n");
-    while (tempPrograms != NULL)
-    {
+    while (tempPrograms != NULL) {
         printf("[%d] \t%s\n", tempPrograms->sequence, tempPrograms->name);
         tempPrograms = tempPrograms->next;
     }
 }
 
-void tasksSwitchPrograms(SubMenu *subMenu)
-{
+void tasksSwitchPrograms(SubMenu *subMenu) {
     printf("Menu -> %s\n", subMenu->name);
 
-    switch (subMenu->sequence)
-    {
-    case 1:
-        underConstruction();
-        break;
-    case 2:
-        underConstruction();
-        break;
-    case 3:
-        underConstruction();
-        break;
-    case 4:
-        viewAllPrograms();
-        waitForClick();
-        break;
+    switch (subMenu->sequence) {
+        case 1:
+            underConstruction();
+            break;
+        case 2:
+            underConstruction();
+            break;
+        case 3:
+            underConstruction();
+            break;
+        case 4:
+            viewAllPrograms();
+            waitForClick();
+            break;
     }
 }
 
-void viewAllSemesters()
-{
+void viewAllSemesters() {
     Semester *tempSemester = db_semesters;
     printf("Sl. \tSemester\n");
-    while (tempSemester != NULL)
-    {
+    while (tempSemester != NULL) {
         printf("[%d] \t%s\n", tempSemester->sequence, tempSemester->name);
         tempSemester = tempSemester->next;
     }
 }
 
-void tasksSwitchSemesters(SubMenu *subMenu)
-{
+void tasksSwitchSemesters(SubMenu *subMenu) {
     printf("%s\n", subMenu->name);
 
-    switch (subMenu->sequence)
-    {
-    case 1:
-        underConstruction();
-        break;
-    case 2:
-        underConstruction();
-        break;
-    case 3:
-        underConstruction();
-        break;
-    case 4:
-        viewAllSemesters();
-        waitForClick();
-        break;
+    switch (subMenu->sequence) {
+        case 1:
+            underConstruction();
+            break;
+        case 2:
+            underConstruction();
+            break;
+        case 3:
+            underConstruction();
+            break;
+        case 4:
+            viewAllSemesters();
+            waitForClick();
+            break;
     }
 }
 
-void viewAllCourses()
-{
+void viewAllCourses() {
     Course *tempCourses = db_courses;
     printf("Sl. \tCode \t\tCredit \tCourse\n");
 
-    while (tempCourses != NULL)
-    {
+    while (tempCourses != NULL) {
         printf("[%d] \t%s \t%d \t\t%s \t\n", tempCourses->sequence, tempCourses->code, tempCourses->credit,
                tempCourses->name);
         tempCourses = tempCourses->next;
     }
 }
 
-void viewAllCoursesFilteredByProgram()
-{
+void viewAllCoursesFilteredByProgram() {
     Course *tempCourses = db_courses;
     viewAllPrograms();
 
@@ -418,10 +397,8 @@ void viewAllCoursesFilteredByProgram()
     scanf("%d", &programSequence);
 
     printf("\nSl. \tCode \t\tCredit \tCourse\n");
-    while (tempCourses != NULL)
-    {
-        if (tempCourses->programSequence == programSequence)
-        {
+    while (tempCourses != NULL) {
+        if (tempCourses->programSequence == programSequence) {
             printf("[%d] \t%s \t%d \t\t%s \t\n", tempCourses->sequence, tempCourses->code, tempCourses->credit,
                    tempCourses->name);
             hasCourses = 1;
@@ -433,7 +410,7 @@ void viewAllCoursesFilteredByProgram()
     if (!hasCourses)
         printf("**This program has no Courses.\n");
 
-viewAgainChoices:
+    viewAgainChoices:
     printf("\nDo you wanna view other programs? [1] for Yes, [0] for No]: ");
     scanf("%d", &viewAgain);
 
@@ -443,69 +420,104 @@ viewAgainChoices:
         goto viewAgainChoices;
 }
 
-void tasksSwitchCourses(SubMenu *subMenu)
-{
+void tasksSwitchCourses(SubMenu *subMenu) {
     printf("%s\n", subMenu->name);
 
-    switch (subMenu->sequence)
-    {
-    case 1:
-        underConstruction();
-        break;
-    case 2:
-        underConstruction();
-        break;
-    case 3:
-        underConstruction();
-        break;
-    case 4:
-        viewAllCourses();
-        waitForClick();
-        break;
-    case 5:
-        viewAllCoursesFilteredByProgram();
-        waitForClick();
-        break;
+    switch (subMenu->sequence) {
+        case 1:
+            underConstruction();
+            break;
+        case 2:
+            underConstruction();
+            break;
+        case 3:
+            underConstruction();
+            break;
+        case 4:
+            viewAllCourses();
+            waitForClick();
+            break;
+        case 5:
+            viewAllCoursesFilteredByProgram();
+            waitForClick();
+            break;
     }
 }
 
-void taskSwitch(Menu *menu, SubMenu *subMenu)
-{
+void viewAllStudents() {
+    Student *tempStudent = db_students;
+    printf("ID \t\t\t\tName \t\t\t\t\tProgram\n");
+
+    while (tempStudent != NULL) {
+        printf("%s \t%s \t\t\t%s\n", tempStudent->id, tempStudent->name, tempStudent->program->name);
+        tempStudent = tempStudent->next;
+    }
+
+    waitForClick();
+}
+
+void tasksSwitchStudents(SubMenu *subMenu) {
+    printf("%s\n", subMenu->name);
+
+    switch (subMenu->sequence) {
+        case 1:
+            underConstruction();
+            break;
+        case 2:
+            underConstruction();
+            break;
+        case 3:
+            underConstruction();
+            break;
+        case 4:
+            viewAllStudents();
+            break;
+        case 5:
+            underConstruction();
+            break;
+        case 6:
+            underConstruction();
+            break;
+        case 7:
+            underConstruction();
+            break;
+    }
+}
+
+void taskSwitch(Menu *menu, SubMenu *subMenu) {
     printf("Menu -> %s\n", menu->name);
 
-    switch (menu->sequence)
-    {
-    case 0:
-        waitForClick();
-        exitProgram();
-        break;
-    case 1:
-        viewAllGrades();
-        waitForClick();
-        break;
-    case 2:
-        tasksSwitchPrograms(subMenu);
-        break;
-    case 3:
-        tasksSwitchSemesters(subMenu);
-        break;
-    case 4:
-        tasksSwitchCourses(subMenu);
-        break;
-    case 5:
-        underConstruction();
-        break;
-    case 6:
-        underConstruction();
-        break;
-    case 7:
-        underConstruction();
-        break;
+    switch (menu->sequence) {
+        case 0:
+            waitForClick();
+            exitProgram();
+            break;
+        case 1:
+            viewAllGrades();
+            waitForClick();
+            break;
+        case 2:
+            tasksSwitchPrograms(subMenu);
+            break;
+        case 3:
+            tasksSwitchSemesters(subMenu);
+            break;
+        case 4:
+            tasksSwitchCourses(subMenu);
+            break;
+        case 5:
+            tasksSwitchStudents(subMenu);
+            break;
+        case 6:
+            underConstruction();
+            break;
+        case 7:
+            underConstruction();
+            break;
     }
 }
 
-void menuOperations()
-{
+void menuOperations() {
     showMenus();
 
     int menuSequence, subMenuSequence;
@@ -514,40 +526,33 @@ void menuOperations()
 
     Menu *selectedMenu = getByMenuSequence(menuSequence);
 
-    if (selectedMenu != NULL)
-    {
+    if (selectedMenu != NULL) {
         SubMenu *selectedSubMenu = NULL;
 
-        if (selectedMenu->subMenus != NULL)
-        {
-        subMenuSelection:
+        if (selectedMenu->subMenus != NULL) {
+            subMenuSelection:
             showSubMenus(selectedMenu);
             printf("\nWhat to do: ");
             scanf("%d", &subMenuSequence);
 
             selectedSubMenu = getBySubMenuSequence(selectedMenu, subMenuSequence);
-            if (selectedSubMenu == NULL)
-            {
+            if (selectedSubMenu == NULL) {
                 printf("\nAn invalid sub-menu is selected.\nPlease choose the correct sub-menu.\n");
                 goto subMenuSelection;
             }
         }
 
         taskSwitch(selectedMenu, selectedSubMenu);
-    }
-    else
-    {
+    } else {
         printf("\nAn invalid menu is selected.\nPlease choose the correct menu.\n");
     }
 }
 
 /*Function Definition Section*/
 
-int main()
-{
+int main() {
     systemInitialization();
-    while (1)
-    {
+    while (1) {
         menuOperations();
     }
 }
